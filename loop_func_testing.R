8a0acebc5180a923cbf052c91d75cb719ea2d4d2
KatieMurenbeeld <katiemurenbeeld@u.boisestate.edu>
2021-04-24 15:47:30 -0600

    minor loop testing

--- loop_func_testing.R
+++ loop_func_testing.R
@@ -10,10 +10,13 @@ projects <- unique(unlist(df$NEPA_PROJECT))
 proj1 <- subset(df, NEPA_PROJECT == projects[1])
 print(proj1)
 
+
+rows = c()
+cols = c()
 y = c()
 legend = c()
 for (i in 1:length(projects)) {
-  #print(projects[i])
+  p <- projects[i]
   #create a temporary dataframe for each project
   test_df <- subset(df, NEPA_PROJECT == projects[i])
   #create a sequence matrix for each project
@@ -22,6 +25,8 @@ for (i in 1:length(projects)) {
   c <- colnames(seq_test)
   test <- combine(r,c)
   y <- append(y, seq_test)
+  rows <- append(rows, r)
+  cols <- append(cols, c)
   legend <- append(legend, test)
   #return(y)
 }
@@ -29,3 +34,9 @@ for (i in 1:length(projects)) {
 print(projects[1])
 proj1_seq <- createSequenceMatrix(proj1$GEN_ACTIVITY, toRowProbs = TRUE, sanitize=TRUE)
 proj1_seq
+
+length(y)
+length(rows)
+length(cols)
+
+test <- merge(y, )
